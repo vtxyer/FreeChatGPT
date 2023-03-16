7f2aa122ee17cc7f2ac613790d3791d166898a27
VTxyer <vtxyer@gmail.com>
2023-03-16 23:23:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://vtxyer.github.io/FreeChatGPT'
+  base: 'https://vtxyer.github.io/FreeChatGPT',
   plugins: [react()],
   resolve: {
     alias: {
